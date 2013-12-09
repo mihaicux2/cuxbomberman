e82e232548f79c588c065bcddd909dc072603114
mihaicux <mihaicux@mihaicux-PC.mshome.net>
2013-12-09 23:52:27 +0200

    Defing structures

--- src/main/java/com/cux/bomberman/BombermanWSEndpoint.java
+++ src/main/java/com/cux/bomberman/BombermanWSEndpoint.java
@@ -7,9 +7,15 @@
 package com.cux.bomberman;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -26,12 +32,18 @@ public class BombermanWSEndpoint {
 
     private static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
     
+    private static boolean isFirst = false;
+    
     @OnMessage
-    public String onMessage(String message) throws IOException {
-        for (Session peer : peers){
-            peer.getBasicRemote().sendText(message);
+    public String onMessage(String message, Session peer) throws IOException {
+        for (Session peer2 : peers){
+            if (!peer.equals(peer2)){
+                peer2.getBasicRemote().sendText(peer.getId()+" : "+message);
+            } else {
+                peer2.getBasicRemote().sendText("back : "+message);
+            }
         }
-        return null;
+        return null; // any string will be send to the requesting peer
     }
 
     @OnClose
@@ -42,10 +54,37 @@ public class BombermanWSEndpoint {
     @OnOpen
     public void onOpen(Session peer) {
         peers.add(peer);
+        if (!isFirst){
+            isFirst = true;
+            watchPeers();
+        }
     }
 
     @OnError
     public void onError(Throwable t) {
+    }
+    
+    public synchronized void watchPeers(){
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        // monitor :-?? bombs, bonuses, events in general 
+                        for (Session peer : peers){
+                            //peer.getBasicRemote().sendText("asd");
+                            //peer.getBasicRemote().sendBinary(ByteBuffer.wrap(message.getBytes()));
+                        }
+                        Thread.sleep(10); // limiteaza la 100FPS comunicarea cu serverul
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(BombermanWSEndpoint.class.getName()).log(Level.SEVERE, null, ex);
+                    }/* catch (IOException ex) {
+                        Logger.getLogger(BombermanWSEndpoint.class.getName()).log(Level.SEVERE, null, ex);
+                    }*/
+                }
+            }
+        }).start();
     }
     
 }
